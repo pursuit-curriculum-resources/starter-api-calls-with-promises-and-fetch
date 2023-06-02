8fd752825b09a1080b379352dbc8635ffbe1ded4
Karolin Rafalski <kr2174@caa.columbia.edu>
2023-06-02 13:06:43 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+
+const form = document.querySelector("form");
+form.addEventListener("submit", getPokemonByID);
+
+function getPokemonByID(e) {
+  e.preventDefault();
+  const input = document.querySelector("#pokemon-id");
+  const id = input.value;
+  fetch(`${BASE_URL}/${id || 1}`)
+    .then((response) => response.json())
+    .then((response) => {
+      displayCard(response);
+    })
+    .catch(displayError);
+  form.reset();
+}
+
+function displayCard(pokemon) {
+  const { name, sprites, order } = pokemon;
+  const section = document.createElement("section");
+  section.classList.add("card");
+  //
+  const img = document.createElement("img");
+  img.setAttribute("src", sprites.front_default);
+  img.setAttribute("alt", name);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = name;
+
+  const paragraph = document.createElement("p");
+  paragraph.textContent = order;
+
+  section.append(img, h2, paragraph);
+  document.querySelector(".pokemon").append(section);
+}
+
+function displayError(error) {
+  const section = document.querySelector("section.error");
+  section.style.display = "block";
+  section.innerHTML = "";
+
+  const paragraph = document.createElement("p");
+  paragraph.textContent = "Something went wrong!";
+
+  const errorMessage = document.createElement("p");
+  errorMessage.classList.add("error-message");
+  errorMessage.textContent = error;
+
+  section.append(paragraph, errorMessage);
+}
